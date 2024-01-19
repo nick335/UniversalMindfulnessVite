c2e5b1d7563444a0b8cdd7821df0cf1041eceaca
nick335 <102188095+nick335@users.noreply.github.com>
2024-01-19 14:28:57 +0100

    delete:removal of testimonial section in aboutpage

--- src/components/about/AboutData.ts
+++ src/components/about/AboutData.ts
@@ -27,10 +27,6 @@ const data = [
     header: 'What will NLP give me?',
     para: 'Mindfulness is a valuable life skill that can help us direct our attention to the present moment with open-minded curiosity and acceptance. Rather than worrying about the past or what might happen in the future, Mindfulness trains us to respond skilfully to whatever is happening right now, without judgment.In essence, Mindfulness is a way of paying attention to, and seeing what is happening in our lives with clarity.But why is this important?.'
   },
-  {
-    header: 'Testimonials',
-    para: 'Mindfulness is a valuable life skill that can help us direct our attention to the present moment with open-minded curiosity and acceptance. Rather than worrying about the past or what might happen in the future, Mindfulness trains us to respond skilfully to whatever is happening right now, without judgment.In essence, Mindfulness is a way of paying attention to, and seeing what is happening in our lives with clarity.But why is this important?.'
-  },
 ]
 
 export default data;
