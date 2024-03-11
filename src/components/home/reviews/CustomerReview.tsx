a092c082beec84ac8810b83b07bc34ed229b5719
nick335 <102188095+nick335@users.noreply.github.com>
2024-03-11 17:33:34 +0100

    fix: customer review fix

--- src/components/home/reviews/CustomerReview.tsx
+++ src/components/home/reviews/CustomerReview.tsx
@@ -14,10 +14,10 @@ const CustomerReview = ({name, img, caption, testimony}: props) => {
       <div className='flex gap-x-[0.77rem] items-start font-inter  lg:gap-x-[1.21rem]'>
         <img src={quote} className='object-fill w-[1.74294rem] h-[1.46281rem] lg:w-[2.80206rem] lg:h-[2.35175rem]' alt='quote' />
         <div>
-          <p className=' text-sm font-medium leading-[1.34456rem] tracking-[-0.01119rem] text-wrap w-full text-textSecondary lg:text-[1.70988rem] lg:leading-[2.16156rem] lg:tracking-[-0.018rem] lg:mt-2'>{testimony}.</p>
+          <p style={{ whiteSpace: 'normal', wordWrap: 'break-word', overflowWrap: 'break-word', WebkitHyphens: 'auto', msHyphens: 'auto' }} className=' text-sm font-medium leading-[1.34456rem] tracking-[-0.01119rem] text-wrap w-full text-textSecondary lg:text-[1.70988rem] lg:leading-[2.16156rem] lg:tracking-[-0.018rem] lg:mt-2'>{testimony}.</p>
           <div className='mt-3.5'>
             <h4 className='text-[0.7005rem] font-bold'>{name}</h4>
-            <h5 className='text-[0.66769rem] font-medium'>{caption}</h5>
+            <h5 className='text-[0.66769rem] font-medium lg:max-w-[90%] text-wrap'>{caption}</h5>
           </div>
         </div>
       </div>
