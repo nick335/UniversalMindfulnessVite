ce59da635a438c7da975c3cbc5258eb1caf18c30
nick335 <102188095+nick335@users.noreply.github.com>
2024-03-24 11:21:41 +0100

    feat: video loop for gallery added!

--- src/components/utility/EmbeddedVideo/EmbeddedVideo.tsx
+++ src/components/utility/EmbeddedVideo/EmbeddedVideo.tsx
@@ -1,68 +1,27 @@
-import  { useRef, useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 
-interface props {
-  videoUrls: string[]
+interface Props {
+  videoUrls: string[];
 }
 
-const EmbeddedVideo = ({ videoUrls }: props) => {
+const EmbeddedVideo: React.FC<Props> = ({ videoUrls }) => {
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
-  const [isInView, setIsInView] = useState(false);
-  const videoRef = useRef<HTMLIFrameElement | null>(null)
 
-
-    const handleVideoEnded = () => {
-      console.log('ended')
-      // Move to the next video in the array or loop back to the first video if at the end
-      setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videoUrls.length);
-    
-      // Update videoRef with the new iframe element (if available)
-      if (videoRef.current) {
-        videoRef.current?.contentDocument?.querySelector('iframe'); // Use optional chaining
-      }
-    };
-
-
-  // Intersection Observer callback function
-  const handleIntersection = (entries: IntersectionObserverEntry[]) => {
-    entries.forEach((entry) => {
-      setIsInView(entry.isIntersecting);
-    });
+  const handleVideoEnded = () => {
+    setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videoUrls.length);
   };
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleIntersection);
-    if (videoRef.current) {
-      observer.observe(videoRef.current);
-    }
-    return () => {
-      observer.disconnect();
-    };
-  }, [videoRef]);
-  useEffect(() => {
-    // Start playing the video when it comes into view
-    if (isInView && videoRef.current) {
-      videoRef.current.contentWindow?.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-    }
-  }, [isInView]);
-
-  // Ensure that the current video index stays within bounds
-  useEffect(() => {
-    if (currentVideoIndex >= videoUrls.length) {
-      setCurrentVideoIndex(0);
-    }
-  }, [currentVideoIndex, videoUrls.length]);
-
   return (
-    <div className='w-full h-full bg-red-300  row-span-2 col-span-2 lg:col-span-2 aspect-square'>
-      <iframe
-        title="Embedded Video"
-        className='h-full w-full'
-        src={`${videoUrls[currentVideoIndex]}`}
-        ref={videoRef}
+    <div className='w-full h-full bg-black  row-span-2 col-span-2 lg:col-span-2 aspect-square '>
+      <ReactPlayer
+        url={videoUrls[currentVideoIndex]}
         onEnded={handleVideoEnded}
-      ></iframe>
+        className='h-full min-h-full max-w-full'
+        controls // Enables controls like play/pause
+      />
     </div>
-  )
-}
+  );
+};
 
-export default EmbeddedVideo
+export default EmbeddedVideo;
