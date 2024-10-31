fcff79747f3c2878e8e100eed42b55e80d02c9f9
Nicholas <102188095+nick335@users.noreply.github.com>
2024-10-31 07:29:27 +0100

    fix: services layout structure

--- src/components/home/services/OurServices.tsx
+++ src/components/home/services/OurServices.tsx
@@ -96,7 +96,7 @@ const OurServices = () => {
   return (
     <section className='mt-[2.92rem] lg:mt-16'>
         <Subheader header='our services' />
-          <motion.div ref={parentRef} variants={parentVariants} initial="hidden" animate={controls} exit="hidden" className='xsm:grid xsm:grid-cols-2 xsm:gap-x-6 md:gap-x-12 lg:gap-x-16 xl:gap-x-20' >
+          <motion.div ref={parentRef} variants={parentVariants} initial="hidden" animate={controls} exit="hidden" className='xsm:grid xsm:grid-cols-2 xsm:gap-x-6 md:gap-x-12 lg:gap-x-16 xl:gap-x-20 w-full' >
             {
               isLoading ?
               <div className='xsm:grid xsm:grid-cols-2 xsm:gap-x-6 md:gap-x-12 lg:gap-x-16 xl:gap-x-20'>
